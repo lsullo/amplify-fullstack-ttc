88c92d4b6c1490df8985f1c9ffa83f3e47fa52e7
lsullo <lsullo2004@gmail.com>
2024-06-15 15:44:47 -0500

    test edit 1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> Working to take care of my people.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
